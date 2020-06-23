fdfa3e878fa8b3b2751d81f37a0efb9fd4b1fbaa
刘浩洋 <haoyang@bitcv.com>
2020-06-23 18:48:20 +0800

    add code

--- t20200531_link/link/main.go
+++ t20200531_link/link/main.go
@@ -45,17 +45,82 @@ func RevertRecursion(cur *Node)  (*Node) {
 	}
 
 	newhead := RevertRecursion(cur.Next)
+	fmt.Println("newHead",newhead)
 	cur.Next.Next = cur
 	cur.Next = nil
 	return newhead
 
 }
+
+func RevertRecursionKGroup(head *Node,num int)  (*Node) {
+	var  nextNode,preNode *Node
+	//下一组开始
+	curGroupStart,nextGroupStart := head,head
+
+	for i:=0;i<num;i++ {
+		if nextGroupStart != nil{
+			nextGroupStart = nextGroupStart.Next
+		}else{
+			return  head
+		}
+	}
+
+	index :=0
+	for curGroupStart != nil && index < num{
+		nextNode = curGroupStart.Next
+		curGroupStart.Next = preNode
+		preNode = curGroupStart
+		curGroupStart = nextNode
+		index++
+	}
+
+	fmt.Println(".....")
+	fmt.Println(nextNode,nextGroupStart)
+	fmt.Println(".....")
+
+	head.Next = RevertRecursionKGroup(nextNode,2)
+	fmt.Println("header",head)
+	return  preNode
+
+}
+
+func reverseKGroup(head *Node, k int) *Node {
+	var pre, next *Node
+	var cur, p *Node = head, head
+	groupSize, index := 0, 0
+	// 将链表的前k个结点组成一组
+	for p != nil && groupSize < k {
+		p = p.Next
+		groupSize++
+	}
+	if groupSize == k {
+		// 翻转该k个结点组成的子链表
+		for cur != nil && index < k {
+			next = cur.Next
+			cur.Next = pre
+			pre = cur
+			cur = next
+			index++
+		}
+		// 继续递归地执行上述过程
+		if next != nil {
+			// head指向子链表翻转后的尾节点
+			head.Next = reverseKGroup(next, k)
+		}
+		// pre记录了为子链表翻转后的头结点
+		return pre
+	} else {
+		// 子链表长度不足k，不翻转
+		return head
+	}
+}
+
+
 func (head  *Node) Print( )  {
 	cur := head
 	for {
-		fmt.Println("print####",cur.Data)
 		cur = cur.Next
-		if(cur == nil){
+		if cur == nil{
 			break
 		}
 	}
@@ -67,17 +132,27 @@ func main()  {
 	head.AddLast(3)
 	head.AddLast(4)
 	head.AddLast(5)
+	head.AddLast(6)
+	head.AddLast(7)
+	head.AddLast(8)
+	head.AddLast(9)
+	head.AddLast(10)
 
-	head.Print()
-	fmt.Println("begin revert....")
-	rHead:= head.Revert()
-	rHead.Print()
-	fmt.Println("begin revert....")
+	//head.Print()
+	//fmt.Println("begin revert....")
+	//rHead:= head.Revert()
+	//rHead.Print()
+	//fmt.Println("begin revert....")
 
-	r2 := RevertRecursion(rHead)
-	r2.Print()
+	//r2 := RevertRecursion(head)
+	//r2.Print()
 	//fmt.Println("end revert....")
 	//
 	//rLink.Print()
 
+	a := RevertRecursionKGroup(head,2)
+	a.Print()
+
+	 //b := reverseKGroup(head,2)
+	 //b.Print()
 }
---
+++ t20200623_lru/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"sync"
+	"container/list"
+	"fmt"
+)
+
+
+type AtomicInt int64
+type LRUCache struct {
+	mutex sync.Mutex
+	size  int
+	cacheList  *list.List
+	cache      map[interface{}]*list.Element
+	hits, gets  AtomicInt
+}
+
+type Entry struct {
+	k interface{}
+	v interface{}
+}
+type EntryImpA struct {
+	k int
+	v string
+}
+
+func NewLruCache(_size int)  LRUCache {
+	lru := LRUCache{
+		size:_size,
+		cacheList:list.New(),
+		cache:make(map[interface{}] *list.Element,_size),
+	}
+	return  lru
+}
+func(lru LRUCache) Get(key string) (interface{},bool)  {
+	if  ele,isOk := lru.cache[key];isOk {
+		lru.cacheList.MoveToFront(ele)
+		return ele.Value.(*Entry).v, true
+	}
+
+	return  nil,false
+}
+
+func (lru LRUCache)Set(key string,value interface{})  {
+	if  ele,isOk := lru.cache[key];isOk{
+		lru.cacheList.MoveToFront(ele)
+		ele.Value = value
+		return
+	}
+
+	ele := lru.cacheList.PushFront(&Entry{k:key,v:value})
+	lru.cache[key]  = ele
+	if lru.cacheList.Len() > lru.size{
+		ele := lru.cacheList.Back()
+		lru.cacheList.Remove(ele)
+		delete(lru.cache,ele.Value.(*Entry).k)
+	}
+}
+
+func main()  {
+	lru :=  NewLruCache(3)
+	lru.Set("1","1")
+	lru.Set("2","2")
+
+	a,_ := lru.Get("1")
+	fmt.Println(a)
+
+}
